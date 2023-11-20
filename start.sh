ba277136383c920ccbaec2c7b81ed8903252a2d5
Erick130601 <104027427+Erick130601@users.noreply.github.com>
2023-11-20 14:36:59 -0500

    cambios

---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+java -jar /usr/local/lib/app.jar --spring.datasource.url=jdbc:mysql://sql10.freemysqlhosting.net:3306/sql10663655 --spring.datasource.username=sql10663655 \
+--spring.datasource.password=RJ9M4uKMyH
