0829a145c3ff14c8f31691e5e4709835d6f961f2
Erick130601 <104027427+Erick130601@users.noreply.github.com>
2023-11-20 15:23:10 -0500

    push

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-java -jar /usr/local/lib/app.jar --spring.datasource.url=jdbc:mysql://sql10.freemysqlhosting.net:3306/sql10663655 --spring.datasource.username=sql10663655 \
---spring.datasource.password=RJ9M4uKMyH
+java -jar /usr/local/lib/app.jar --spring.datasource.url=jdbc:postgresql://dpg-cldrmrfgsrdc738ssgg0-a.oregon-postgres.render.com:5432/database_autoya --spring.datasource.username=user \
+--spring.datasource.password=Lkscaa0s0bih2eCXoQhMrF82v9BORDn7
